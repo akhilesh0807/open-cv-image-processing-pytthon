042fa15a5ba47bbe2615391306c661aa89ba21a8
akhilesh0807 <akhilesh.kamble0807@gmail.com>
2017-07-13 01:10:46 +0530

    Update resize.py
    this a program to resize any image , just enter the (pixel)width of the image you want and it will  resize it , it will  look after the aspect ratio of the image

--- resize.py
+++ resize.py
@@ -6,7 +6,7 @@ img = cv2.imread('chem.jpg')
 # not look skewed or distorted -- therefore, we calculate
 # the ratio of the new image to the old image
 r = (enter the width you want)/ img.shape[1]
-dim = (enter width , int(img.shape[0]*r))
+dim = (enter width you want, int(img.shape[0]*r))
  
 # perform the actual resizing of the image and show it
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
